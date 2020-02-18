9830af8918cf58042a0ed3a7712c10682c6c7819
Luis Urrea <luis.urrea.lo@outlook.com>
2020-02-17 22:47:26 -0300

    Modificamos la ruta del SW

--- js/sw-utils.js
+++ js/sw-utils.js
@@ -1,12 +1,24 @@
 
-// Guardar en cache dinámico
-function actualizaCacheDinamico(dynamicCache, req, res){
-    if(res.ok){
-        return caches.open(dynamicCache).then(cache => {
-            cache.put(req, res.clone());
+
+// Guardar  en el cache dinamico
+function actualizaCacheDinamico( dynamicCache, req, res ) {
+
+
+    if ( res.ok ) {
+
+        return caches.open( dynamicCache ).then( cache => {
+
+            cache.put( req, res.clone() );
+            
             return res.clone();
+
         });
-    }else{
+
+    } else {
         return res;
     }
+
+
+
 }
+
